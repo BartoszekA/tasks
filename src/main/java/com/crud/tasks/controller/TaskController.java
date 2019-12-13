74d840cd264578d3dbb2487913fe0f2d27faa34d
BartoszekA <agnieszka.bartoszek@gmail.com>
2019-12-13 23:36:31 +0100

    commit

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin(origins = "*")
+
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
@@ -29,7 +31,7 @@ public class TaskController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(@RequestParam Long taskId) throws TaskNotFoundException {
+    public void deleteTask(@RequestParam Long taskId) {
         service.deleteTask(taskId);
     }
 
